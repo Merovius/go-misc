6be7d64ea07e907ee91c19552c1b71592eb0f63a
Axel Wagner <mail@merovius.de>
2015-07-17 18:54:00 +0200

    lazy: Add package

---
+++ lazy/doc.go
@@ -0,0 +1,7 @@
+// Package lazy provides lazy evaluation for builtin types.
+//
+// Most code in this package is automatically generated with
+// merovius.de/go-misc/cmd/go-lazy.
+package lazy
+
+//go:generate go-lazy -out lazy.go
